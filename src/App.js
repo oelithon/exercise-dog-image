4c4cb4c743ad23516394b8491e042a9f140dcb91
Elithon Silva <elithonsilva@gmail.com>
2021-09-10 20:07:26 -0300

    impl: requisição da API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageDog: '',
+    };
+
+    this.fetchAPI = this.fetchAPI.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const dogAPI = await fetch('https://dog.ceo/api/breeds/image/random');
+    const dogAPIJson = await dogAPI.json();
+    this.setState({
+      imageDog: dogAPIJson,
+    });
+  }
+
+  render() {
+    const { imageDog: { message } } = this.state;
+    return (
+      <div>
+        <h1>Dog Image Ramdon</h1>
+        <img src={ message } alt="imagem de cachorros" />
+      </div>
+    );
+  }
+}
 
 export default App;
